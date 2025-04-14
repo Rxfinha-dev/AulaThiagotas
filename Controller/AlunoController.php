1207de093f57ca3ead212e13326d526d9babe645
RAFAEL OLIVEIRA <rafael.oliveira901@etec.sp.gov.br>
2025-04-14 15:51:26 -0300

    view e delete

--- Controller/AlunoController.php
+++ Controller/AlunoController.php
@@ -51,12 +51,19 @@
 
         public static function delete():void
         {
-            
+            parent::isProtected();
 
+            $model = new Aluno();
 
+            try{
+                $model->delete( (int) $_GET['id']);
+                parent::redirect("/aluno");
+            }catch(Exception $e){
+                $model->setError("Ocorreu um erro ao excluir o aluno.");
+                $model->setError($e->getMessage());
+            }
 
-
-            
+            parent::render('Aluno/lista_aluno.php', $model);            
         }
     }
  ?>
